986461ac0946a0c8a43a31b9ba31006a7982629e
최진석 <79374648+choiapple@users.noreply.github.com>
2022-07-28 14:14:52 +0900

    Create Cart.java

---
+++ ssg/src/main/java/com/busan/ssg/cart/domain/Cart.java
@@ -0,0 +1,29 @@
+package com.busan.ssg.cart.domain;
+
+import com.busan.ssg.product.domain.Product;
+import com.busan.ssg.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
