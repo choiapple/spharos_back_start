fac1115774205cf6ab056cb3aa942802264a02a5
최진석 <79374648+choiapple@users.noreply.github.com>
2022-08-02 12:16:17 +0900

    Create Product.java

---
+++ mvc-study/src/main/java/com/studybusan/mvc/Product/model/Product.java
@@ -0,0 +1,21 @@
+package com.studybusan.mvc.Product.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int price;
+    private String titleImgUrl;
+    private String des;
+    private String made;
+}
