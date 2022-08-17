e68503401700109edcf01eafb16a8656ebb24aaa
choiapple <cj5255@naver.com>
2022-08-17 13:42:20 +0900

    Create JpaRepositoryQueryApplication.java

---
+++ jpa repository query (1)/jpa-repository-query/src/main/java/union/JpaRepositoryQueryApplication.java
@@ -0,0 +1,13 @@
+package union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRepositoryQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRepositoryQueryApplication.class, args);
+	}
+
+}
