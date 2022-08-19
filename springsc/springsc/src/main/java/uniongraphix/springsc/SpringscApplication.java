0e6d255e7d22fe03750b84ef94bfb2060628d07a
choiapple <cj5255@naver.com>
2022-08-19 11:56:45 +0900

    Create SpringscApplication.java

---
+++ springsc/springsc/src/main/java/uniongraphix/springsc/SpringscApplication.java
@@ -0,0 +1,13 @@
+package uniongraphix.springsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringscApplication.class, args);
+	}
+
+}
