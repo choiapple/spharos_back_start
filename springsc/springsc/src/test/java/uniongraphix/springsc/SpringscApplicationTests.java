50d26e3269be81b1f100e0bcdd731f999724f7b1
choiapple <cj5255@naver.com>
2022-08-19 11:56:48 +0900

    Create SpringscApplicationTests.java

---
+++ springsc/springsc/src/test/java/uniongraphix/springsc/SpringscApplicationTests.java
@@ -0,0 +1,13 @@
+package uniongraphix.springsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
